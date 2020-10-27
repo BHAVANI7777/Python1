3f488781b509e1292a719f2bcc39c45388b332f0
Bhavani Koyada <60246218+BHAVANI7777@users.noreply.github.com>
2020-10-26 20:54:35 -0700

    Update RollDice.py

--- RollDice.py
+++ RollDice.py
@@ -2,11 +2,14 @@ import random
 
 def roll(min, max):
     while True:
-           print("Rolling the dices...")
-           print(f"Your number is {random.randint(min, max)}")
-           answer = input("Do you want to roll the dices again? (y/n) ")
-           if answer.lower() == 'n':
-            break
-           elif answer.lower() != 'n' and answer.lower() != 'y':
-                print("Please Enter y/n ")
-roll(1, 6)  
+            answer = input("Do you want to roll the dices ? (y/n) ")
+            if answer.lower() == 'n':
+                break
+            elif answer.lower() != 'n' and answer.lower() != 'y':
+                print("Enter valid letter...")
+            else:
+                print("Rolling the dices...")
+                print(f"Your number is {random.randint(min, max)}")
+
+
+roll(1, 6)
